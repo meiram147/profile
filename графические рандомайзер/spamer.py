9f95411c8cc48e939e98c23c705e15ee6bd5dda1
meiram147 <75033568+meiram147@users.noreply.github.com>
2022-04-16 17:24:46 +0600

    Create spamer.py

---
+++ графические рандомайзер/spamer.py
@@ -0,0 +1,38 @@
+import pyautogui as pg 
+import time 
+
+
+time.sleep(10)
+for i in range(50):
+    pg.write("tz18021996")
+    pg.press("Enter")        
+        
+            
+                
+                    
+                        
+                            
+                                
+                                    
+                                        
+                                            
+                                                
+                                                    
+                                                        
+                                                            
+                                                                
+                                                                    
+                                                                        
+                                                                            
+                                                                                
+                                                                                    
+                                                                                        
+                                                                                            
+                                                                                                
+                                                                                                    
+                                                                                                        
+                                                                                                            
+                                                                                                                
+                                                                                                                    
+                                                                                                                        
+                                                
